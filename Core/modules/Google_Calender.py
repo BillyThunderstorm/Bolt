a601a0fb7c3291548c94aeb6e14f5ce9e2a8d990
ThunderstormBilly <billycarteriv@gmail.com>
2026-08-08 06:59:26 -0500

    Cleanup: remove misspelled duplicate Google_Calender.py

--- Core/modules/Google_Calender.py
+++
@@ -1,7 +0,0 @@
-"""Back-compat alias for the misspelled module name.
-
-Prefer importing from modules.Google_Calendar. This shim keeps older
-call sites working after the Jul 2026 rename.
-"""
-
-from .Google_Calendar import *  # noqa: F401,F403
